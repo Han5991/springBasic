8bf49054a6d77eef04066da16f3b5e6084ac17d9
rewq985 <bnbt3@naver.com>
2021-11-20 18:35:58 +0900

    @Primary 우선권을 가지게 한다. @Qualifier 빈 이름을 지정하여 사용한다. (귀찮다. 지저분 하고)

--- src/main/java/hello/core/member/Member.java
+++ src/main/java/hello/core/member/Member.java
@@ -1,5 +1,10 @@
 package hello.core.member;
 
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
 public class Member {
 
     private Long id;
@@ -10,38 +15,5 @@ public class Member {
         this.id = id;
         this.name = name;
         this.grade = grade;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Grade getGrade() {
-        return grade;
-    }
-
-    public void setGrade(Grade grade) {
-        this.grade = grade;
-    }
-
-    @Override
-    public String toString() {
-        return "Member{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", grade=" + grade +
-                '}';
     }
 }
