232148ed8890a5b497e94a79d2e0e0474bb3187d
rewq985 <bnbt3@naver.com>
2021-11-22 20:53:18 +0900

    라이프사이클 상기시키자

---
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,26 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+    @Test
+    public void lifeCycleTest(){
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = context.getBean(NetworkClient.class);
+        System.out.println(client);
+        context.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig{
+        @Bean
+        public NetworkClient networkClient(){
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("test");
+            return networkClient;
+        }
+    }
+}
