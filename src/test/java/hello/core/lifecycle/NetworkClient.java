d248d257e0e5ad2c331570ac841d44ec677dd390
rewq985 <bnbt3@naver.com>
2021-11-23 23:17:17 +0900

    @PostConstruct     @PreDestroy 이걸 사용하자 좋다 단점 외부 라이브러리에는 사용 못 한다 그럼 @Bean에 메소드 등록해서 사용하자

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -2,17 +2,19 @@ package hello.core.lifecycle;
 
 import lombok.ToString;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @ToString
 public class NetworkClient {
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
+        System.out.println("NetworkClient.setUrl");
         this.url = url;
     }
 
@@ -28,5 +30,18 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disConnect() {
         System.out.println("close = " + url);
+    }
+
+    @PostConstruct
+    public void init() throws Exception {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
+        disConnect();
     }
 }
