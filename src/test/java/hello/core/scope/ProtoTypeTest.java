0dc9c86b9f2f566ba69b7e93c5d5592df19800fb
rewq985 <bnbt3@naver.com>
2021-11-24 21:04:17 +0900

    스코프 공부 거진 싱글톤을 많이 씀 근데 프로토타입(종료 메서드 호출이 안 됨)같이 쓰면 문제가 생김

---
+++ src/test/java/hello/core/scope/ProtoTypeTest.java
@@ -0,0 +1,41 @@
+package hello.core.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProtoTypeTest {
+
+    @Test
+    void ProtoTypeBeanFind(){
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProtoTypeBean.class);
+
+        ProtoTypeBean protoTypeBean1 = context.getBean(ProtoTypeBean.class);
+        ProtoTypeBean protoTypeBean2 = context.getBean(ProtoTypeBean.class);
+
+        System.out.println("protoTypeBean1 = " + protoTypeBean1);
+        System.out.println("protoTypeBean2 = " + protoTypeBean2);
+
+        assertThat(protoTypeBean1).isNotSameAs(protoTypeBean2);
+
+        context.close();
+    }
+
+    @Scope("prototype")
+    static class ProtoTypeBean{
+        @PostConstruct
+        public void init(){
+            System.out.println("ProtoTypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("ProtoTypeBean.destroy");
+        }
+    }
+}
