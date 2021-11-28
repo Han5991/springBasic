8abdd61b2abf8c71b274342df9c9eb527ebe8744
rewq985 <bnbt3@naver.com>
2021-11-28 10:50:08 +0900

    uuid 로 고객의 요청을 찍어둔다 근대 스프링 서버가 올라가면서 요청이 있지도 않은 요청을 달라하니 에러가 생김
    - Provider 해결 하자
    - 인터셉터로도 구현이 가능하다

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,29 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+    private final MyLogger myLogger;
+    private final LogDemoService logDemoService;
+
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request){
+        String requestURI = request.getRequestURI();
+        myLogger.setRequestURL(requestURI);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+
+        return "OK";
+    }
+}
